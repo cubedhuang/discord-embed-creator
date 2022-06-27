3699f827ebcdda407e3590d7379411e67ee137d3
cubedhuang <99danielh@gmail.com>
2022-06-27 01:34:07 -0400

    fix: move head into _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
 import "../styles/globals.scss";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	const title = "Discord Embed Creator";
+	const description =
+		"A tool to build Discord embeds for discord.js and discord.py.";
+
+	return (
+		<>
+			<Head>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+				<meta name="theme-color" content="#000000" />
+
+				<title>{title}</title>
+				<meta property="og:title" content={title} />
+				<meta name="description" content={description} />
+				<meta property="og:description" content={description} />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
